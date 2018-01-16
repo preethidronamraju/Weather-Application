ad78c2a1487f90a74dd5aa00c3c69c64ed83a35a
Preethi <dronamraju.preethi93@gmail.com>
2018-01-15 18:11:50 -0600

    all other endpoints added

--- src/main/java/com/rest/preethi/weather/controller/WeatherController.java
+++ src/main/java/com/rest/preethi/weather/controller/WeatherController.java
@@ -2,13 +2,12 @@ package com.rest.preethi.weather.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.rest.preethi.weather.model.Weather;
 import com.rest.preethi.weather.service.WeatherService;
+
+import java.util.List;
 
 @RestController
 public class WeatherController {
@@ -18,33 +17,48 @@ public class WeatherController {
 
 
     @RequestMapping(value = "/weather/updateWeather", method = RequestMethod.POST)
-    public ResponseEntity<?> updateWeather(@RequestBody Weather weather){
-    	weatherService.updateWeather(weather);
-    	
-	        return ResponseEntity.status(200).build();
-	    }
-    @RequestMapping(value = "/weather/listOfCities", method = RequestMethod.GET)
-    public ResponseEntity<?> listOfCities(){
-        return ResponseEntity.status(200).build();
+    public Weather updateWeather(@RequestBody Weather weather){
+	        return weatherService.updateWeather(weather);
     }
-    @RequestMapping(value = "/weather/givenCityWeather", method = RequestMethod.GET)
-    public ResponseEntity<?> getGivenCityWeather(){
-        return ResponseEntity.status(200).build();
+    @RequestMapping(value = "/weather/listOfCities", method = RequestMethod.GET)
+    public List<String> listOfCities(){
+        return weatherService.listOfCities();
+    }
+    @RequestMapping(value = "/weather/latestCityWeather", method = RequestMethod.GET)
+    public Weather getLatestCityWeather(@RequestParam(required=true) String city){
+        return weatherService.getLatestCityWeather(city);
     }
 
-    @RequestMapping(value = "/weather/givenCityWeatherInfo", method = RequestMethod.GET)
-    public ResponseEntity<?> getGivenCityWeatherInfo(){
-        return ResponseEntity.status(200).build();
+    @RequestMapping(value = "/weather/givenCityWeatherInfo/{city}", method = RequestMethod.GET)
+    public String getGivenCityWeatherInfo(@RequestParam(required=true) String property, @PathVariable String city){
+
+        Weather weather = weatherService.getLatestCityWeather(city);
+        if(property.equalsIgnoreCase("humidity")){
+            return weather.getHumidity()+"";
+        }
+        else if(property.equalsIgnoreCase("pressure")){
+            return weather.getPressure()+"";
+        }
+        else if(property.equalsIgnoreCase("temperature")){
+            return weather.getTemperature()+"";
+        }
+        else if(property.contains("speed")){
+            return weather.getWind().getSpeed()+"";
+        }
+        else if(property.contains("degree")){
+            return weather.getWind().getSpeed()+"";
+        }
+        return "not found";
     }
 
     @RequestMapping(value = "/weather/hourlyAvgWeather", method = RequestMethod.GET)
-    public ResponseEntity<?> getHourlyAvgWeather(){
-        return ResponseEntity.status(200).build();
+    public List<Weather> getHourlyAvgWeather(@RequestParam(required=true) String city){
+        return weatherService.getDailyAverageWeather(city);
     }
 
     @RequestMapping(value = "/weather/dailyAvgWeather", method = RequestMethod.GET)
-    public ResponseEntity<?> getDailyAvgWeather(){
-        return ResponseEntity.status(200).build();
+    public List<Weather>  getDailyAvgWeather(@RequestParam(required=true) String city){
+        return weatherService.getDailyAverageWeather(city);
     }
 
     @RequestMapping(value = "/checkServer", method = RequestMethod.GET)
--- src/main/java/com/rest/preethi/weather/service/WeatherService.java
+++ src/main/java/com/rest/preethi/weather/service/WeatherService.java
@@ -1,10 +1,16 @@
 package com.rest.preethi.weather.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.stereotype.Service;
 
 import com.rest.preethi.weather.dao.WeatherDAO;
 import com.rest.preethi.weather.model.Weather;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class WeatherService {
@@ -16,6 +22,32 @@ public class WeatherService {
         return weatherDAO.save(weather);
     }
 
+    public List<String> listOfCities(){
 
-   
+        List<Weather> allList = weatherDAO.findAll();
+        List<String> cities = new ArrayList<String>();
+        for (Weather weather: allList) {
+            if(!cities.contains(weather.getCity())){
+                cities.add(weather.getCity());
+            }
+        }
+        return cities;
+    }
+
+    public Weather getLatestCityWeather(String city){
+        List<Weather> allList = weatherDAO.findAll();
+        List<Weather> allCities = allList.stream().filter(p -> p.getCity().equalsIgnoreCase(city)).collect(Collectors.toList());
+        allCities.sort(Comparator.comparing(Weather::getTimestamp));
+        Weather result = new Weather();
+        if(allCities.size() > 0)
+            result = allCities.get(0);
+        return result;
+    }
+
+    public List<Weather> getDailyAverageWeather(String city){
+        List<Weather> allList = weatherDAO.findAll();
+        List<Weather> allCities = allList.stream().filter(p -> p.getCity().equalsIgnoreCase(city)).collect(Collectors.toList());
+        allCities.sort(Comparator.comparing(Weather::getTimestamp));
+        return allCities;
+    }
 }
